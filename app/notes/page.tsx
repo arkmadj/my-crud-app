cf0c00ba9037c8e074a543236d377b189558d9fe
arkmadj <ahmadjinadu@gmail.com>
2022-12-22 19:59:00 +0100

    Added get notes function

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,3 +1,9 @@
+async function getNotes() {
+  const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=20')
+  const data = await res.json();
+  return data?.items as any[]
+}
+
 export default async function NotesPage(){
   return (
     <div>
